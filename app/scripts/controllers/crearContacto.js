a3fd027d93be8fbd7202815e0b53dd0828fd3f04
Alberto Cole <acpii2005@gmail.com>
2013-05-14 10:17:18 -0600

    Agregando Modal de Crear Contacto

--- app/scripts/controllers/crearContacto.js
+++ app/scripts/controllers/crearContacto.js
@@ -11,10 +11,31 @@ angular.module('77DigitalAngularDemoApp')
       'Guanacaste',
       'Limon',
       'Puntarenas'
-    ];    
+    ];
+
+    $scope.openModal = function () {
+      $scope.modalContacto = true;
+    };
+
+    $scope.closeModal = function () {
+      $scope.modalContacto = false;
+    };
+
+    $scope.modalOpts = {
+      backdropFade: true,
+      dialogFade:true
+    };
+
     $scope.guardarContacto = function(){
-      contactoService.guardarContacto($scope.contactoForm).then(function(data){
-        console.log(data);
+      contactoService.guardarContacto($scope.contactoForm).then(function(data){        
+        if(data.msg === "OK!"){          
+          $scope.contactoForm = {provincia: "San Jose"};
+          $scope.modalMessage = "Se ha creado un nuevo contacto";
+          $scope.openModal();
+        }else if(data.msg === "FAIL"){
+          $scope.modalMessage = "La creacion de un contacto ha fallado";
+          $scope.openModal();
+        }
       });
     };
   }]);
